17d73094a9e3a7e4f55e3558fc069f5666e35f0e
Damian Swarts <damianswarts2019@gmail.com>
2024-03-05 16:53:46 +0200

    Added a function that allows the user to change the size of the grid when they click the button and made a few changes to the createGrid function to accomodate the new function

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-/* Create 15 more container divs to go with the one already 
-in my HTML file*/
-for (let i = 0; i < 16; i++){
-    const moreContainers = document.createElement('div');
-    moreContainers.classList = "container";
-    const body = document.getElementById('body').appendChild(moreContainers);
-}
+const body = document.getElementById('body');
+const grid = document.getElementById("main container");
 
-// Create 16 div elements for each of the containers
-const container = document.querySelectorAll(".container");
-container.forEach(function (container){
-    for (let j = 0; j < 16; j++){
-        const div = document.createElement('div');
-        div.classList = "grid-cell";
-        container.appendChild(div);       
+/* Create container divs for the display of the grid*/
+function createGrid(size){
+    for (let i = 1; i <= size; i++){
+        const moreContainers = document.createElement('div');
+        moreContainers.classList = "container";
+        grid.appendChild(moreContainers);
     }
-});
 
-const gridCell = document.querySelectorAll(".grid-cell")
-// Event listener to change the color of a cell when the user hovers over it
-gridCell.forEach(function (cell){
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "yellowgreen";
+    const container = document.querySelectorAll(".container");
+    container.forEach(function (container){
+        for (let j = 1; j <= size; j++){
+            const div = document.createElement('div');
+            div.classList = "grid-cell";
+            container.appendChild(div);       
+        }
     });
-});
+
+    const gridCell = document.querySelectorAll(".grid-cell")
+    // Event listener to change the color of a cell when the user hovers over it
+    gridCell.forEach(function (cell){
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "yellowgreen";
+        });
+    });
+}
+createGrid(16);
+
+// Function that changes the size of the grid depending on the size the user wants it
+function changeGridSize() {
+    grid.innerHTML = "";
+    const size = Number(prompt("How many rows do you want the grid to be? (NO MORE THAN 100)"));
+    if (size <= 100 && size >= 0){
+        createGrid(size);
+    }
+}
